a7ad4870010a8bc946beb6de79dccd5c2116b7de
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2024-03-13 21:40:27 +0000

    Updated String service

--- credential-generator/src/services/StringService.ts
+++ credential-generator/src/services/StringService.ts
@@ -1,30 +1,28 @@
 import { CharacterType, getRange } from "../enumerations/CharacterType";
 import { INumberService } from "./NumberService";
 
-export interface IStringService
-{
-    addSpacesToCamelCase(camelCaseValue: string):string;
-    generateString(length:number, type: CharacterType):string;
+export interface IStringService {
+    addSpacesToCamelCase(camelCaseValue: string): string;
+    getRandomCharacter(type: CharacterType): string;
+    generateString(length: number, type: CharacterType): string;
 }
 
-export class StringService {
-    private numberService:INumberService;
+export class StringService implements IStringService {
+    private numberService: INumberService;
 
-    constructor(numberService:INumberService) {
+    constructor(numberService: INumberService) {
         this.numberService = numberService;
     }
 
-    addSpacesToCamelCase(camelCaseValue: string)
-    {
+    addSpacesToCamelCase(camelCaseValue: string) {
         var str = "";
         const range = getRange(CharacterType.UpperCase);
-        for(let i=0;i < camelCaseValue.length;i++)
-        {
+        for (let i = 0; i < camelCaseValue.length; i++) {
             let character = camelCaseValue[i];
             const charCode = character.charCodeAt(0);
             const result = i > 0 && charCode >= range[0] && charCode <= range[1];
-            
-            if(result) {
+
+            if (result) {
                 str += " ";
                 character = character.toLocaleLowerCase();
             }
@@ -35,42 +33,45 @@ export class StringService {
         return str;
     }
 
-    generateString(length:number, type: CharacterType):string {
-        let str = "";
-        const isMixed = type == CharacterType.Mixed || type == CharacterType.MixedNoSymbols;
-        let range = isMixed
-         ? []
-         : getRange(type);
+    getRandomCharacter(type: CharacterType):string {
+        const isMixed = type == CharacterType.Mixed
+            || type == CharacterType.MixedNoSymbols;
+        
+        let range = isMixed ? [] : getRange(type);
 
-        while(str.length < length)
-        {
-            if(!isMixed) {
-                if(range.length == 2)
-                {
-                    str += String.fromCharCode(this.numberService.getRandomRange(range[0], range[1]));
-                }
-                else throw "Unable to determine range";
-            }
-            else
-            {
-                const setRangeMaximum = type == CharacterType.MixedNoSymbols ? 7500 : 10000;
-                const mixerValue = this.numberService.getRandomInt(setRangeMaximum);
-                if(mixerValue < 2500)
-                {
-                    str += this.generateString(1, CharacterType.LowerCase);
-                }
-                else if(mixerValue > 2500 && mixerValue <= 5000)
-                {
-                    str += this.generateString(1, CharacterType.UpperCase);
-                }
-                else if(mixerValue > 5000 && mixerValue <= 7500)
-                {
-                    str += this.generateString(1, CharacterType.Numeric);
-                }
-                else {
-                    str += this.generateString(1, CharacterType.SpecialCharacters);
-                }
-            }
+        if (!isMixed && range.length !== 2) {
+            throw "Unable to determine range";
+        }
+        
+        if (!isMixed) {
+            return String
+                .fromCharCode(this.numberService
+                    .getRandomRange(range[0], range[1]));
+        }
+
+        const setRangeMaximum = type == CharacterType.MixedNoSymbols
+            ? 7500
+            : 10000;
+        const mixerValue = this.numberService.getRandomInt(setRangeMaximum);
+        if (mixerValue < 2500) {
+            return this.getRandomCharacter(CharacterType.LowerCase);
+        }
+        else if (mixerValue > 2500 && mixerValue <= 5000) {
+            return this.getRandomCharacter(CharacterType.UpperCase);
+        }
+        else if (mixerValue > 5000 && mixerValue <= 7500) {
+            return this.getRandomCharacter(CharacterType.Numeric);
+        }
+        else {
+            return this.getRandomCharacter(CharacterType.SpecialCharacters);
+        }
+    }
+
+    generateString(length: number, type: CharacterType): string {
+        let str = "";
+        
+        while (str.length < length) {
+            str += this.getRandomCharacter(type);
         }
 
         return str;
