946d66d8139688d953bfbd63f5ae1dbad3d790c7
romesh-abeyawardena <romesh.abeyawardena@bellrockgroup.co.uk>
2024-03-14 13:36:57 +0000

    Added Luxon

--- credential-generator/src/components/CredentialList.vue
+++ credential-generator/src/components/CredentialList.vue
@@ -2,14 +2,23 @@
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import { useCredentialStore } from '../stores/CredentialStore';
+import { DateTime } from "luxon";
 
 const store = useCredentialStore();
+
+function formateDate(value:Date) {
+    return DateTime.fromISO(value.toISOString()).toFormat("dd MMM yyyy HH:mm");
+}
 </script>
 <template>
 <DataTable :value="store.credentials">
     <Column field="id" header="ID"></Column>
     <Column field="username" header="User name"></Column>
     <Column field="password" header="Password"></Column>
-    <Column field="created" header="Created"></Column>
+    <Column field="created" header="Created">
+        <template #body="slotProps">
+            {{ formateDate(slotProps.data.created) }}
+        </template>
+    </Column>
 </DataTable>
 </template>
