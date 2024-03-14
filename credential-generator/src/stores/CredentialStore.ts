800da13861aefae2970bd9c2becbcf1e37abfe21
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2024-03-14 22:02:10 +0000

    Fixed build

--- credential-generator/src/stores/CredentialStore.ts
+++ credential-generator/src/stores/CredentialStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import { ICredential } from "../credential";
+import { ICredential } from "../credential/index";
 import { ref, Ref } from "vue";
 import { v4 } from "uuid";
 export interface ICredentialStore {
